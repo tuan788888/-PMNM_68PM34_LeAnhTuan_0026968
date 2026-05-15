1373cb071c959365bc5a020ede35e05931d933ec
tuan788888 <tle263036@gmail.com>
2026-05-15 12:25:59 +0000

    FIRST COMMIT

---
+++ app/controllers/home.php
@@ -0,0 +1,11 @@
+<?php 
+class home{
+    public function index(){
+        echo "Day la trang chu";
+    }
+    public function create(){
+        echo "Day la trang gioi thieu";
+    }
+
+}
+?>
---
+++ app/controllers/sinhvien.php
@@ -0,0 +1,10 @@
+<?php
+class sinhvien {
+    public function index() {
+        echo "Hello from sinhvien controller!";
+    }
+    public function create() {
+        echo "This is the create method in sinhvien controller.";
+    }
+}
+?>
